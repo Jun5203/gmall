eff28e74ff35d63e31e37752152d10bddeca2cba
tom <tom@qq.com>
2020-04-27 11:17:51 +0800

    加入用户管理模块

---
+++ gmall_user_manage/src/test/java/com/itcast/gmall/user/GmallUserManageApplicationTests.java
@@ -0,0 +1,15 @@
+package com.itcast.gmall.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallUserManageApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
